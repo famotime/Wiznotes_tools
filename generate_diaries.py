c37a38c7b5418b533c92fbba645b87bd67a54f14
Quincy Zou <quincy.zou@gmail.com>
2022-06-02 21:40:28 +0800

    逐月生成日记模板

---
+++ generate_diaries.py
@@ -0,0 +1,25 @@
+"""逐月生成日记模板"""
+import pathlib
+from datetime import date, datetime
+import calendar
+
+
+year = 2022
+path = pathlib.Path.cwd() / 'diaries'
+template = path / 'diary_template.md'
+
+with open(template, encoding='utf-8') as f:
+    lines = f.readlines()
+    base_content = ''.join(lines[1:])
+
+for month in range(1, 13):
+    day_count = calendar.monthrange(year, month)[1]
+    month_content = f'# {year}年{month}月日记\n\n'
+    for day in range(1, day_count+1):
+        today = date(year, month, day)
+        first_line = f"## {today} {datetime.strftime(today, '%A')[:3]} 晴\n"
+        content = first_line + base_content + '\n\n'
+        month_content += content
+
+    with open(path / f'{year}-{month}.md', 'w', encoding='utf-8') as f:
+        f.write(month_content)
